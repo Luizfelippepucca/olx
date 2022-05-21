9e78b33136994d58b90bb00f1c044d17b9503e1c
luiz pucca <lu1z_21@hotmail.com>
2022-05-21 19:59:48 -0300

    create-header

--- src/components/partials/header/index.jsx
+++ src/components/partials/header/index.jsx
@@ -1,7 +1,43 @@
-import {HeaderArea} from './styles';
 
-const Header = ()=>{
-    return <HeaderArea>...</HeaderArea>
+import { Link} from 'react-router-dom';
+import IsLogged, { isLogged } from '../../../helpers/authHandler';
+import { HeaderArea, Container, LogoArea ,LogoLetter1, LogoLetter2, LogoLetter3,NavMenu,ButtonMenu} from './styles';
+
+
+const Header = () =>{
+    const logged = isLogged();
+    return (
+        <HeaderArea>
+            <Container>
+                <LogoArea>
+                   <Link to="/">
+                       <LogoLetter1>O</LogoLetter1>
+                       <LogoLetter2>L</LogoLetter2>
+                       <LogoLetter3>X</LogoLetter3>
+                   </Link>
+                </LogoArea>
+                <NavMenu>
+                    <ul>
+                        {!logged &&
+                            <>
+                             <li><Link to="/signin">Login</Link></li>
+                             <li><Link to="/signup">Cadastrar</Link></li>
+                            </>
+                        }
+                       {logged && 
+                       <>
+                        <li><Link to="/my-account">Minha conta</Link></li>
+                        <li><Link to="/logout">Sair</Link></li>
+                        <li><ButtonMenu>Poste um anúncio</ButtonMenu></li>
+                       </>
+                       } 
+                      
+                    </ul>
+                </NavMenu>
+            </Container>
+        
+        </HeaderArea>
+    );
 }
 
 export default Header;
--- src/components/partials/header/styles.js
+++ src/components/partials/header/styles.js
@@ -1,5 +1,82 @@
 import styled from 'styled-components';
-export const HeaderArea= styled.div`
-height:100px;
-background-color:#d3d3d3 ;
+
+export const HeaderArea = styled.div`
+height:60px;
+background-color:#fff;
+border:1px solid #ccc;
+a{
+    text-decoration:none ;
+}
 `
+export const Container = styled.div`
+ max-width:1400px ;
+ display: flex ;
+ margin:auto ;
+
+`; 
+
+export const LogoArea = styled.div`
+display: flex ;
+flex:1 ;
+align-items:center ;
+height: 60px;
+margin-left:10px ;
+`;
+
+export const LogoLetter1 = styled.span`
+font-size: 27px ;
+font-weight:bold ;
+color:#ff0000;
+`;
+
+export const LogoLetter2 = styled.span`
+font-size: 27px ;
+font-weight:bold ;
+color:#00ff00;
+`;
+
+export const LogoLetter3 = styled.span`
+font-size: 27px ;
+font-weight:bold ;
+color:#0000ff;
+`;
+
+export const NavMenu = styled.nav`
+display : flex ;
+align-items:center ;
+height:60px ;
+margin-right:10px ;
+
+
+ ul{
+    list-style: none;
+    display: flex ;
+    align-items:center ;
+
+    li{
+        margin: 0px 20px;
+
+        a{
+            color:#000;
+            font-size: 14px;
+            &:hover{color:#999}
+        }
+    }
+ }
+`;
+
+export const ButtonMenu = styled.button`
+outline: none ;
+border: 0;
+background-color:#ff8100 ;
+border-radius:4px ;
+color:#fff ;
+padding: 5px 10px ;
+cursor: pointer;
+
+&:hover{
+    background-color:#e57706;
+    transform: scale(1.1) ;
+    transition: 0.3s ease-in ;
+}
+`;
